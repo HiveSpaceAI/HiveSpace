76dfc1152cf012826513f59854c242fe3c400fb9
HiveSpaceDev <klnotkiq@veridicalmail.com>
2024-09-19 16:03:19 -0400

    Create index.js

---
+++ packages/backend/native/index.js
@@ -0,0 +1,13 @@
+import { createRequire } from 'node:module';
+
+const require = createRequire(import.meta.url);
+
+/** @type {import('.')} */
+const binding = require('./server-native.node');
+
+export const mergeUpdatesInApplyWay = binding.mergeUpdatesInApplyWay;
+export const verifyChallengeResponse = binding.verifyChallengeResponse;
+export const mintChallengeResponse = binding.mintChallengeResponse;
+export const getMime = binding.getMime;
+export const Tokenizer = binding.Tokenizer;
+export const fromModelName = binding.fromModelName;
