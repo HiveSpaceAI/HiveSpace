4e85ebe7a01fb68cf8000bb1847d44d36b2509f6
HiveSpaceDev <klnotkiq@veridicalmail.com>
2024-09-19 16:10:01 -0400

    Create app.controller.ts

---
+++ packages/backend/server/src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get } from '@nestjs/common';
+
+import { Public } from './core/auth';
+import { Config, SkipThrottle } from './fundamentals';
+
+@Controller('/')
+export class AppController {
+  constructor(private readonly config: Config) {}
+
+  @SkipThrottle()
+  @Public()
+  @Get()
+  info() {
+    return {
+      compatibility: this.config.version,
+      message: `AFFiNE ${this.config.version} Server`,
+      type: this.config.type,
+      flavor: this.config.flavor,
+    };
+  }
+}
