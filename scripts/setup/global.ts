b75024fc163354fbb14ea41ed18f41d9599f05cf
HiveSpaceDev <klnotkiq@veridicalmail.com>
2024-09-19 16:19:27 -0400

    Create global.ts

---
+++ scripts/setup/global.ts
@@ -0,0 +1,14 @@
+import { getRuntimeConfig } from '@affine/cli/src/webpack/runtime-config';
+import { setupGlobal } from '@wasper/env/global';
+
+globalThis.runtimeConfig = getRuntimeConfig({
+  distribution: 'browser',
+  mode: 'development',
+  channel: 'canary',
+});
+
+if (typeof window !== 'undefined') {
+  window.location.search = '?prefixUrl=http://127.0.0.1:3010/';
+}
+
+setupGlobal();
